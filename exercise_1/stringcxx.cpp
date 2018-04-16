f5374235876d2d3e777cf3019d8baaf90f722e65
Dang Huynh <hhdang@partners.accedian.com>
2018-04-16 09:04:06 +0700

    add source code and Makefile

---
+++ exercise_1/stringcxx.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "stringcxx.h"
+
+
+
+int main(){
+	char str[100];
+	char str1[100];
+	char *strp;
+	
+	printf("Nhap chuoi 1: ");
+	gets(str);
+	printf("Nhap chuoi 2: ");
+	gets(str1);
+	
+	//printf("%s",str);
+	
+	//char *ch;
+	
+	//ch=ass(str);
+	
+	//printf("%s",ch);
+	
+	//if(compare(str,str1)){
+		//printf("2 chuoi giong nhau");
+	//}else {
+		//printf("2 chuoi khac nhau");
+	//}
+	
+	strp=concen(str,str1);
+	
+	printf("%s",strp);
+	
+	return 0;
+	
+
+}
---
+++ exercise_1/stringcxx.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+char *ass(char *c){
+	char *b;
+	b = c;
+	return b;
+}
+
+int compare (char *str1, char *str2){
+	int i=0;	
+	while(*(str1+i)!='\0'){
+		if(*(str1+i)!=*(str2+i)){
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
+char *concen(char *str, char *str1){
+	char *strp;
+	
+	int n=0,m=0;
+	
+	while(*(str+n)!='\0'){
+		n++;	
+	}
+	
+	while(*(str1+m)!='\0'){
+		m++;
+	}
+
+	strp = (char*)malloc((n+m)*sizeof(char));
+	
+	int i=0;
+	while(i<n){
+		*(strp + i) = *(str + i);
+		i++;
+	}
+	int j=0;
+	while(i<(n+m)){
+		*(strp + i) = *(str1 + j);
+		i++;
+		j++;
+	}
+	
+	
+	return strp;
+}
